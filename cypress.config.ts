c1b95261d57c007a20ad2e46881f564604aceaca
Brayden Beavis <bray@Braydens-MBP.lan>
2023-03-15 10:18:56 +1100

    config restructure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'ysm2wi',
   e2e: {
     baseUrl: 'http://localhost:3000',
-    env: {
-        projectId: 'ysm2wi',
-      },
     fixturesFolder: 'cypress/fixtures/*',
     supportFile: 'cypress/support/component.ts',
     specPattern: 'cypress/e2e/*'
