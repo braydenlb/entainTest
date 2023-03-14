9a39963ec126b2f86c99ef4b4c4af994db8b8832
Brayden Beavis <bray@Braydens-MacBook-Pro.local>
2023-03-14 20:54:40 +1100

    webpack updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+module.exports = {
+    // ...other config options...
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  };
