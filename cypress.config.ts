1c71c9125159004235c0b114516246324a5d3b6f
Brayden Beavis <bray@Braydens-MacBook-Pro.local>
2023-03-14 21:07:12 +1100

    cypress config update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "cypress";
 
+module.exports = {
+  projectId: "ysm2wi",
+  // The rest of the Cypress config options go here...
+}
+
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000/",
