04b7cbb1252dd4e19edc7401b6dfe11ec95ecf02
Brayden Beavis <bray@Braydens-MacBook-Pro.local>
2023-03-14 21:00:17 +1100

    additional content tests

--- cypress/e2e/content.cy.ts
+++ cypress/e2e/content.cy.ts
@@ -1,5 +1,6 @@
 import { RACING_CATEGORIES } from "../config/constants";
 
+
 describe('Page Content', () => {
   it('Should correctly display page title', () => {
     cy.visit('');
@@ -18,5 +19,15 @@ describe('Page Content', () => {
         });
       })
     });
+  })
+
+  it('Handles errors gracefully', () => {
+    cy.intercept('GET', 'http://localhost:3000/v2/racing/next-races-category-group?count=5&categories=%5B%224a2788f8-e825-4d36-9894-efd4baf1cfae%22%2C%229daef0d7-bf3c-4f50-921d-8e818c60fe61%22%2C%22161d9be2-e909-4326-8c2c-35ed71fb460b%22%5D', {
+      statusCode: 500,
+      body: 'Something went wrong',
+    }).as('getRacesError')
+  
+    cy.visit('/')
+    cy.wait('@getRacesError')
   })
 });
