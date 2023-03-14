5b9c80da116832cfd506e4ef7c5f1020dc71ab74
Brayden Beavis <bray@Braydens-MacBook-Pro.local>
2023-03-14 20:55:10 +1100

    added filter tests

--- cypress/e2e/filters.cy.ts
+++ cypress/e2e/filters.cy.ts
@@ -1,7 +1,38 @@
 describe('Category Filters', () => {
-  it.skip('Insert test here');
+  it('Cycle through filters and ensure they are working', () => {
+    cy.visit('');
+    cy.wait(500);
 
-  it.skip('Insert test here');
+    //for each checkbox, uncheck is and then check that the dom items have updated
+    cy.get('.filter-checkbox').each(($el) => {
+      cy.wrap($el).each(($child) => {
+        const targetSelector = '#root > div > header > div > div > div:nth-child(3) > div:nth-child(1)';
+      const originalText = Cypress.$(targetSelector).text();
+    
+      $child.trigger('click');
+      cy.wait(1000);
+    
+      cy.get(targetSelector).should(($target) => {
+        const newText = $target.text();
+        expect(newText).not.to.equal(originalText);
+      });
+      })
+    });
+  });
 
-  it.skip('Insert test here');
+  it('Should display only 5 class=item elements', () => {
+    cy.visit('');
+    cy.wait(500);
+  
+    // Assert that there are 5 class=item elements on the page
+    cy.get('.item').should('have.length', 5);
+  
+    // Click on a filter checkbox
+    cy.get('.filter-checkbox').first().click();
+    cy.wait(1000);
+  
+    // Assert that there are still only 5 class=item elements on the page
+    cy.get('.item').should('have.length', 5);
+  });
+
 });
